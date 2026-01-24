20d6db2268bec02fa84a010dd04aaf48889979b1
Keven Grillo <kevengrillo@gmail.com>
2026-01-24 10:03:25 -0300

    User SqlServer and first Migration

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -22,7 +22,6 @@ namespace SalesWebMvc
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -31,7 +30,6 @@ namespace SalesWebMvc
                     options.UseSqlServer(Configuration.GetConnectionString("SalesWebMvcContext")));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -41,7 +39,6 @@ namespace SalesWebMvc
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
