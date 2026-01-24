c3f6cba0edfb12ab3ca0757c5f7ee6d12859ac72
Keven Grillo <kevengrillo@gmail.com>
2026-01-24 12:31:43 -0300

    Change theme - Other Entities and Second Migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,8 +1,29 @@
-﻿namespace SalesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+
+        public Department() 
+        { 
+        } 
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public double TotalSalles(DateTime Initial, DateTime final) 
+        { 
+            return Sellers.Sum(Seller => Seller.TotalSales(Initial, final)); 
+        }
     }
 }
